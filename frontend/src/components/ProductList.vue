68b1bddc6b071a563ba2358b4b44fd4528b10ee1
Pierre Jarry <pierre.jarry82@gmail.com>
2022-04-14 15:17:58 +0200

    Fetch products using axios and Vuex and display a basic list of the products on the frontend

--- frontend/src/components/ProductList.vue
+++ frontend/src/components/ProductList.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="products">
       <h1>{{ title }}</h1>
+      <ul v-if="allProducts">
+          <li v-for="product in allProducts" :key="product.id">
+              {{ product.productName }}
+          </li>
+      </ul>
+      <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+
 export default {
     name: "ProductList",
     data() {
         return {
             title: "Product list"
         }
+    },
+    methods: {
+        ...mapActions(['fetchProducts']),
+    },
+    computed: mapGetters(['allProducts']),
+    created() {
+        this.fetchProducts()
     }
 }
 </script>
--- frontend/src/store/modules/products.js
+++ frontend/src/store/modules/products.js
@@ -1,17 +1,27 @@
+import axios from 'axios'
+
 const state = {
-    
+    products: []
 }
 
 const getters = {
-    
+    allProducts: (state) =>  state.products.filter( product => product.stock > 0)
 }
 
 const actions = {
-    
+    async fetchProducts({ commit }) {
+        try {
+            const response = await axios.get('http://localhost:3000/grocery')
+            commit('SET_PRODUCTS', response.data);
+        }
+        catch (error) {
+            console.log(error)
+        }
+    },
 }
 
 const mutations = {
-    
+    SET_PRODUCTS: (state, products) => (state.products = products),
 }
 
 export default {
